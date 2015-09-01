7882ff97a58b422ca3feddabe3a6acdadf76c55b
fsilva <fsilva@stone.com.br>
2015-09-01 15:07:58 -0300

    Atualização do SDK.

---
+++ SDKdemo/StoneSDK-Demo/src/br/com/stonesdk/SendEmailActivity.java
@@ -0,0 +1,83 @@
+package br.com.stonesdk;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import stone.application.interfaces.StoneCallbackInterface;
+import stone.database.transaction.TransactionObject;
+import stone.providers.SendEmailProvider;
+import stone.utils.EmailClient;
+
+public class SendEmailActivity extends ActionBarActivity {
+
+	TextView userText;
+	TextView sendText;
+	EditText userEditText;
+	EditText sendEmailText;
+	Button sendButton;
+	
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_send_email);
+		userText = (TextView) findViewById(R.id.emailDescription);
+		sendText = (TextView) findViewById(R.id.emailSendDescription);
+		userEditText = (EditText) findViewById(R.id.emailUserText);
+		sendEmailText = (EditText) findViewById(R.id.emailSendText);
+		sendButton = (Button) findViewById(R.id.sendEmailButton);
+		
+		instanceEvents();
+	}
+
+	private void instanceEvents() {
+		sendButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				// captura o texto inserido pelo usuario
+				String emailTextSample = userEditText.getText().toString();
+				String emailTo = sendEmailText.getText().toString().trim();
+				
+				// sdk aqui.
+				EmailClient emailClient = new EmailClient("smtp.office365.com", 
+														  "noreply@stone.com.br", 
+														  "8]5Ah}aGxC",
+														  emailTo, 
+														  "Comprovante Stone 2.0");
+				emailClient.setSport("587");
+				emailClient.setSmtpPport("587");
+				String receipt = emailTextSample; 
+				// emailClient.receiptAsHtml(getApplicationContext(), ((TransactionObject) transactionListView.getAdapter().getItem((int) id)).getIdFromBase());
+				SendEmailProvider sendEmailProvider = new SendEmailProvider(SendEmailActivity.this, emailClient, receipt);
+				sendEmailProvider.setWorkInBackground(false);
+				sendEmailProvider.setDialogMessage("Enviando comprovante..");
+				sendEmailProvider.setConnectionCallback(new StoneCallbackInterface() {
+					public void onSuccess() {
+						Toast.makeText(getApplicationContext(), "Enviado com sucesso", Toast.LENGTH_LONG).show();
+					}
+
+					public void onError() {
+						Toast.makeText(getApplicationContext(), "Nao enviado", Toast.LENGTH_LONG).show();
+					}
+				});
+				sendEmailProvider.execute();
+			}
+		});
+	}
+
+	public boolean onCreateOptionsMenu(Menu menu) {
+		return true;
+	}
+
+	public boolean onOptionsItemSelected(MenuItem item) {
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
--- SDKdemo/StoneSDK-Demo/src/br/com/stonesdk/TransactionListActivity.java
+++ SDKdemo/StoneSDK-Demo/src/br/com/stonesdk/TransactionListActivity.java
@@ -2,9 +2,13 @@ package br.com.stonesdk;
 
 import android.support.v7.app.ActionBarActivity;
 
+import java.security.PublicKey;
 import java.util.List;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.bluetooth.BluetoothDevice;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -56,25 +60,39 @@ public class TransactionListActivity extends ActionBarActivity implements OnItem
 		return super.onOptionsItemSelected(item);
 	}
 
-	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-		
-		String[] arrayIdAtPositionZero = listView.getAdapter().getItem(position).toString().split("=");
-		final int transacionId = Integer.parseInt(arrayIdAtPositionZero[0].trim());
-		
-		final CancellationProvider cancellationProvider = new CancellationProvider(TransactionListActivity.this, transacionId);
-		cancellationProvider.setWorkInBackground(false);
-		cancellationProvider.setDialogMessage("Cancelando...");
-		cancellationProvider.setConnectionCallback(new StoneCallbackInterface() {
-			
-			public void onSuccess() {
-				Toast.makeText(getApplicationContext(), cancellationProvider.getMessageFromAuthorize(), 1).show();
-				finish();
-			}
-			
-			public void onError() {
-				Toast.makeText(getApplicationContext(), "Um erro ocorreu durante o cancelamento com a transacao de id:" + transacionId, 1).show();
-			}
-		});
-		cancellationProvider.execute();
+	public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.list_dialog_title)
+        	   .setMessage(R.string.list_dialog_message)
+               .setPositiveButton(R.string.list_dialog_print, new DialogInterface.OnClickListener() {
+                   public void onClick(DialogInterface dialog, int id) {
+                       // lógica da impressão
+                	   Toast.makeText(getApplicationContext(), "Em breve...", 1).show();
+                   }
+               })
+               .setNegativeButton(R.string.list_dialog_cancel, new DialogInterface.OnClickListener() {
+                   public void onClick(DialogInterface dialog, int id) {
+                       	// lógica do cancelamento
+                	   	String[] arrayIdAtPositionZero = listView.getAdapter().getItem(position).toString().split("=");
+               			final int transacionId = Integer.parseInt(arrayIdAtPositionZero[0].trim());
+               		
+               			final CancellationProvider cancellationProvider = new CancellationProvider(TransactionListActivity.this, transacionId);
+               			cancellationProvider.setWorkInBackground(false); // para dar feedback ao usuario ou nao.
+               			cancellationProvider.setDialogMessage("Cancelando...");
+               			cancellationProvider.setConnectionCallback(new StoneCallbackInterface() { // chamada de retorno.
+               				public void onSuccess() {
+               					Toast.makeText(getApplicationContext(), cancellationProvider.getMessageFromAuthorize(), 1).show();
+               					finish();
+               				}
+               				
+               				public void onError() {
+               					Toast.makeText(getApplicationContext(), "Um erro ocorreu durante o cancelamento com a transacao de id:" + transacionId, 1).show();
+               				}
+               			});
+               			cancellationProvider.execute();
+                   }
+               });
+        builder.create();
+		builder.show();
 	}
 }
